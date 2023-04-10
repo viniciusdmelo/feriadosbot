03b19828a50133a2e7788c6743cc619803f0d7da
Vinicius de Melo <114265969+viniciusdmelo@users.noreply.github.com>
2023-04-10 17:42:36 -0300

    Create sugestoessp.py

---
+++ sugestoessp.py
@@ -0,0 +1,14 @@
+import random
+
+sugestoes = [
+    "conhecer o Museu de Arte de São Paulo - MASP",
+    "visitar o Parque Ibirapuera e faça um piquenique",
+    "fazer uma caminhada pela Avenida Paulista",
+    "visitar o Mercado Municipal",
+    "conhecer o Jardim Botânico",
+    "fazer uma visita guiada ao Theatro Municipal",
+    "passear pelo bairro da Liberdade e experimente a culinária japonesa"
+]
+
+# Escolher uma sugestão aleatória
+sugestao_aleatoria = random.choice(sugestoes)
