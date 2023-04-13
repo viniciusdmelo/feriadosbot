bb4e155458b6b0402a70d07b4d1d52b29d8cd0b7
Vinicius de Melo <114265969+viniciusdmelo@users.noreply.github.com>
2023-04-13 20:53:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -54,42 +54,13 @@ def telegram_bot():
   chat_id = update["message"]["chat"]["id"]
   message = update["message"]["text"]
 
-  mensagens = []
-  for update in dados:
-    lastupdate_id = update["update_id"]
-    first_name = update["message"]["from"]["first_name"]
-    sender_id = update["message"]["from"]["id"]
-    if "text" not in update["message"]:
-      continue
-    message = update["message"]["text"].upper()
-    chat_id = update["message"]["chat"]["id"]
-    datahora = datetime.datetime.fromtimestamp(update["message"]["date"])
-    if "username" in update["message"]["from"]:
-      username = f'@{update["message"]["from"]["username"]}'
-    else:
-      username = ""
-    print(f"[{datahora}] Nova mensagem de {first_name} @{username} ({chat_id}): {message}")
-    mensagens.append([str(datahora), "recebida", username, first_name, chat_id, message])
+  if message == "/START":
+    texto_resposta = "Olá! Seja bem-vindo(a)! \nVocê quer saber quando é o próximo feriado em São Paulo? Digite SIM, caso queira."
+  elif message == "SIM":
+    texto_resposta = f"O próximo feriado é dia {prox_feriado_formatado}. Aproveite para ir a lugares como o Parque Ibirapuera, o museu da Pinacoteca ou passear pela Liberdade."
+  else:
+    texto_resposta = "Não consegui processar sua mensagem. Ainda estou aprendendo :("
+  nova_mensagem = {"chat_id": chat_id, "text": texto_resposta}
 
-  #APÓS RECEBER AS RESPOSTAS E ARMAZENÁ-LAS, O BOT RESPONDE CONFORME A MENSAGEM ENVIADA
-
-    if message == "/START":
-      texto_resposta = "Olá! Seja bem-vindo(a)! \nVocê quer saber quando é o próximo feriado em São Paulo? Digite SIM, caso queira."
-    elif message == "/SIM":
-      texto_resposta = f"O próximo feriado é dia {prox_feriado_formatado}. \nVocê gostaria do texto automatizado? Digite /TEXTO, caso queira."
-    elif message == "/TEXTO":
-      texto_resposta = f"Título: Aproveite o feriado de {descricao_feriado} em São Paulo! \nTexto: Que tal aproveitar o feriado de {descricao_feriado} em São Paulo? Uma sugestão de atividade é: {sugestao_aleatoria}. Além disso, você também pode aproveitar para {random.choice(sugestoes)} e {random.choice(sugestoes)}."
-    else:
-      texto_resposta = "Não consegui processar sua mensagem. Ainda estou aprendendo :("
-    nova_mensagem = {"chat_id": chat_id, "text": texto_resposta}
-    requests.post(f"https://api.telegram.org/bot{token}/sendMessage", data=nova_mensagem)
-    mensagens.append([str(datahora), "enviada", username, first_name, chat_id, texto_resposta])
-    
-  resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
-  print(resposta.text)
+  requests.post(f"https://api.telegram.org/bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
   return "ok"
-
-  #ADICIONA TODAS AS MENSAGENS ENVIADAS NA PLANILHA E ATUALIZADA O ID DO ÚLTIMO UPDATE
-
-  sheet.append_rows(mensagens)
-  sheet.update("A1", lastupdate_id)
