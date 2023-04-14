aa3350e3ec27812e5fe0b5dadf908523fc086f22
Vinicius de Melo <114265969+viniciusdmelo@users.noreply.github.com>
2023-04-14 00:50:06 -0300

    Create sugestoespe.py

---
+++ sugestoespe.py
@@ -0,0 +1,14 @@
+import random
+
+sugestoes = [
+"visitar o Centro Histórico de Recife e conhecer a história da cidade",
+"fazer um passeio de catamarã pelos rios Capibaribe e Beberibe",
+"conhecer o Instituto Ricardo Brennand e suas coleções de arte e armas",
+"visitar o Parque das Esculturas de Francisco Brennand",
+"passear pelo Mercado de São José e experimentar a culinária local",
+"visitar a praia de Boa Viagem e aproveitar o mar",
+"conhecer o Alto da Sé em Olinda e apreciar a vista da cidade"
+]
+
+#ESCOLHER UMA SUGESTÃO ALEATORIAMENTE
+sugestao_aleatoria_pe = random.choice(sugestoes)
