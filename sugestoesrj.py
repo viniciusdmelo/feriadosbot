909e406c4e61745e2c9cbd8476af521c4cba87b3
Vinicius de Melo <114265969+viniciusdmelo@users.noreply.github.com>
2023-04-14 00:48:00 -0300

    Create sugestoesrj.py

---
+++ sugestoesrj.py
@@ -0,0 +1,14 @@
+import random
+
+sugestoes = [
+"conhecer o Cristo Redentor no topo do Corcovado",
+"visitar o Pão de Açúcar e apreciar a vista panorâmica da cidade",
+"fazer uma caminhada pela orla de Copacabana",
+"visitar o Jardim Botânico",
+"conhecer o Museu de Arte do Rio - MAR",
+"fazer um passeio de barco pela Baía de Guanabara",
+"visitar o bairro de Santa Teresa e apreciar sua arquitetura colonial"
+]
+
+#ESCOLHER UMA SUGESTÃO ALEATORIAMENTE
+sugestao_aleatoria_rj = random.choice(sugestoes)
