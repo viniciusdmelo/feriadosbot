f9c97a168ce191e6aa11df0797e40fa716169e34
Vinicius de Melo <114265969+viniciusdmelo@users.noreply.github.com>
2023-04-14 00:49:08 -0300

    Create sugestoespr.py

---
+++ sugestoespr.py
@@ -0,0 +1,14 @@
+import random
+
+sugestoes = [
+"conhecer o Jardim Botânico de Curitiba",
+"visitar o Parque Tanguá e apreciar a vista",
+"fazer uma caminhada pelo centro histórico de Curitiba",
+"visitar o Museu Oscar Niemeyer",
+"conhecer a Ópera de Arame",
+"passear pelo bairro de Santa Felicidade e experimentar a gastronomia italiana",
+"visitar o Bosque Alemão e a Torre dos Filósofos"
+]
+
+#ESCOLHER UMA SUGESTÃO ALEATORIAMENTE
+sugestao_aleatoria_pr = random.choice(sugestoes)
