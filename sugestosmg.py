e44bef639197d7bdd21392dae0951b27db74cd7c
Vinicius de Melo <114265969+viniciusdmelo@users.noreply.github.com>
2023-04-14 00:53:05 -0300

    Create sugestosmg.py

---
+++ sugestosmg.py
@@ -0,0 +1,14 @@
+import random
+
+sugestoes_mg = [
+"conhecer a Praça da Liberdade e seus prédios históricos",
+"visitar o Mercado Central e experimentar a comida mineira",
+"fazer uma caminhada pela Lagoa da Pampulha e apreciar a arquitetura de Oscar Niemeyer",
+"visitar o Museu de Arte da Pampulha",
+"conhecer o Conjunto Arquitetônico da Pampulha",
+"fazer uma visita guiada ao Palácio das Artes",
+"passear pelo bairro de Savassi e conhecer seus bares e restaurantes"
+]
+
+#ESCOLHER UMA SUGESTÃO ALEATORIAMENTE
+sugestao_aleatoria_mg = random.choice(sugestoes_mg)
