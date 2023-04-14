c6a02fcfd2624f4fae9e87d52f084a5ec22b9386
Vinicius de Melo <114265969+viniciusdmelo@users.noreply.github.com>
2023-04-14 00:55:53 -0300

    Create sugestoesce.py

---
+++ sugestoesce.py
@@ -0,0 +1,14 @@
+import random
+
+sugestoes_ce = [
+"visitar a Praia de Iracema e apreciar a vista do mar",
+"fazer uma caminhada pelo calçadão da Avenida Beira-Mar",
+"conhecer o Mercado Central e experimentar a culinária local",
+"visitar o Centro Dragão do Mar de Arte e Cultura",
+"passear pelo Parque do Cocó e aproveitar a natureza",
+"visitar o Museu da Imagem e do Som",
+"conhecer a Feirinha da Beira-Mar aos domingos"
+]
+
+#ESCOLHER UMA SUGESTÃO ALEATORIAMENTE
+sugestao_aleatoria_ce = random.choice(sugestoes_ce)
