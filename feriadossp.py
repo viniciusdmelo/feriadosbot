851cb89a1ac68d739db797c8805aa10d4986e4c1
Vinicius de Melo <114265969+viniciusdmelo@users.noreply.github.com>
2023-04-14 01:01:41 -0300

    Update feriadossp.py

--- feriadossp.py
+++ feriadossp.py
@@ -48,8 +48,6 @@ prox_feriado = tabela_final.loc[tabela_final['Data'] > pd.Timestamp.now(), 'Data
 descricao_feriado_sp = tabela_final.loc[tabela_final['Data'] == prox_feriado, 'Comemoração'].iloc[0]
 prox_feriado_formatado_sp = prox_feriado.strftime('%d/%m/%Y')
 
-print(f'O próximo feriado é {descricao_feriado_sp}, em {prox_feriado_formatado_sp}.')
-
 #AUTORIZANDO GRAVAR OS DADOS EM UMA PLANILHA NO GOOGLE SHEETS
 GOOGLE_SHEETS_CREDENTIALS = os.environ["GOOGLE_SHEETS_CREDENTIALS"]
 with open("credenciais.json", mode="w") as arquivo:
