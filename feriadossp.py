94168608316d2f2cbcdc3ef106d2ded3ce610d38
Vinicius de Melo <114265969+viniciusdmelo@users.noreply.github.com>
2023-04-13 22:50:00 -0300

    Update feriadossp.py

--- feriadossp.py
+++ feriadossp.py
@@ -1,14 +1,12 @@
 import os
-
 import requests
 import gspread
 import pandas as pd
 import datetime
-
 from bs4 import BeautifulSoup
 from oauth2client.service_account import ServiceAccountCredentials
 
-#FAZENDO O SCRAPPING DOS DADOS DO SITE FERIADOS.COM.BR
+# FAZENDO O SCRAPPING DOS DADOS DO SITE FERIADOS.COM.BR
 url_ano_atual = 'https://www.feriados.com.br/feriados-sao_paulo-sp.php'
 site_ano_atual = requests.get(url_ano_atual)
 conteudo_ano_atual = site_ano_atual.content
@@ -31,14 +29,14 @@ for linha in datas_comemorativas_ano_atual:
     ajuste = linha.split(' - ')
     ajuste_feriados_ano_atual.append(ajuste)
 
-#CRIAÇÃO DE UM DATAFRAME COM AS DATAS COMEMORATIVAS
+# CRIAÇÃO DE UM DATAFRAME COM AS DATAS COMEMORATIVAS
 tabela_datas_comemorativas_ano_atual = pd.DataFrame(ajuste_feriados_ano_atual, columns=['Data', 'Comemoração'])
 tabela_final = tabela_datas_comemorativas_ano_atual.drop_duplicates(subset='Data', keep='first').sort_values('Data')
 
-#DESCOBRINDO QUE DIA É HOJE
+# DESCOBRINDO QUE DIA É HOJE
 today = datetime.date.today().strftime('%d/%m/%Y')
 
-#DESCOBRINDO QUANDO É O PRÓXIMO FERIADO
+# DESCOBRINDO QUANDO É O PRÓXIMO FERIADO
 prox_feriado = tabela_final.loc[tabela_final['Data'] > today, 'Data'].iloc[0]
 descricao_feriado = tabela_final.loc[tabela_final['Data'] == prox_feriado, 'Comemoração'].iloc[0]
 prox_feriado_formatado = datetime.datetime.strptime(prox_feriado, '%d/%m/%Y').strftime('%d/%m/%Y')
@@ -48,11 +46,12 @@ print(f'O próximo feriado é {descricao_feriado}, em {prox_feriado_formatado}.'
 
 GOOGLE_SHEETS_CREDENTIALS = os.environ["GOOGLE_SHEETS_CREDENTIALS"]
 with open("credenciais.json", mode="w") as arquivo:
-  arquivo.write(GOOGLE_SHEETS_CREDENTIALS)
+    arquivo.write(GOOGLE_SHEETS_CREDENTIALS)
 conta = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json")
 api = gspread.authorize(conta)
 planilha = api.open_by_key("1zI16LZUgnR-1Xr3MqsjdV6wtyYNMiPpVuxdUVoXYuA4")
 sheet = planilha.worksheet("Pandas")
 
 lista = tabela_final.values.tolist()
+sheet.clear()
 sheet.insert_rows(lista)
