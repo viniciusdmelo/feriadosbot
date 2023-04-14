5747e23934bc5cc7a72671daf334ee16b21ca803
Vinicius de Melo <114265969+viniciusdmelo@users.noreply.github.com>
2023-04-14 00:57:56 -0300

    Create sugestoesam.py

---
+++ sugestoesam.py
@@ -0,0 +1,14 @@
+import random
+
+sugestoes_am = [
+"visitar o Teatro Amazonas e conhecer a história de Manaus",
+"fazer uma caminhada pelo Centro Histórico de Manaus",
+"conhecer o Mercado Municipal Adolpho Lisboa e experimentar a culinária local",
+"visitar o Museu do Índio",
+"passear pelo Parque Municipal do Mindu e aproveitar a natureza",
+"visitar o Palácio da Justiça",
+"conhecer a Praia da Ponta Negra e aproveitar o rio Negro"
+]
+
+#ESCOLHER UMA SUGESTÃO ALEATORIAMENTE
+sugestao_aleatoria_am = random.choice(sugestoes_am)
