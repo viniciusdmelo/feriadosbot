9378918a102a1f932f1841e650159235c3d6ef02
Vinicius de Melo <114265969+viniciusdmelo@users.noreply.github.com>
2023-04-14 00:53:43 -0300

    Create sugestoesgo.py

---
+++ sugestoesgo.py
@@ -0,0 +1,14 @@
+import random
+
+sugestoes_go = [
+"conhecer o Parque Flamboyant e suas áreas verdes",
+"visitar o Centro Cultural Oscar Niemeyer",
+"fazer uma caminhada pelo Bosque dos Buritis",
+"visitar o Museu Pedro Ludovico Teixeira",
+"conhecer a Feira Hippie aos domingos",
+"passear pelo Parque Vaca Brava e aproveitar a natureza",
+"visitar o Memorial do Cerrado"
+]
+
+#ESCOLHER UMA SUGESTÃO ALEATORIAMENTE
+sugestao_aleatoria_go = random.choice(sugestoes_go)
