26711a7d3f460b6ab37d41df2cc04a90f89fc2b2
Vinicius de Melo <114265969+viniciusdmelo@users.noreply.github.com>
2023-04-14 00:54:50 -0300

    Create sugestoesdf.py

---
+++ sugestoesdf.py
@@ -0,0 +1,14 @@
+import random
+
+sugestoes_df = [
+"visitar o Congresso Nacional e conhecer a arquitetura de Oscar Niemeyer",
+"fazer uma caminhada pelo Eixo Monumental",
+"conhecer a Catedral Metropolitana de Brasília",
+"visitar o Museu Nacional Honestino Guimarães",
+"passear pelo Jardim Botânico de Brasília",
+"conhecer o Memorial JK",
+"visitar o Palácio do Itamaraty"
+]
+
+#ESCOLHER UMA SUGESTÃO ALEATORIAMENTE
+sugestao_aleatoria_df = random.choice(sugestoes_df)
